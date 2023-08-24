ef855b2f1ce3e606a58ecae224beeeb187565f76
KARTIK9 <ktidke3@gmail.com>
2023-08-24 23:49:02 +0530

    services page updated

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,19 +1,21 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from "react";
 
 // eslint-disable-next-line react/prop-types
-const ServiceCard = ({heading, para}) => {
+const ServiceCard = ({ heading, para, image }) => {
   return (
     <div>
-        <div className='flex flex-col cursor-pointer bg-orange-300 rounded-lg w-[373px] h-[475px] mt-10 max-sm:w-[300px] hover:bg-orange-100'>
-            <img className='rounded-lg' src={"https://wallpapers.com/images/featured/6tlr4cfeg3q0al8n.jpg"} alt='card-image' />
-            <div className='p-5  overflow-auto'>
-                <h1 className='text-[20px] font-inter font-bold'>{heading}</h1>
-                <p>{para}</p>
-            </div>
+      <div className="flex flex-col cursor-pointer bg-orange-300 w-[600px] rounded-t-lg h-[475px] mt-10 max-sm:w-[300px] hover:bg-orange-100">
+        <div className="h-[20rem] object-fill">
+          <img className="rounded-t-lg relative w-[100%] h-[100%] " src={image} alt="card-image" />
         </div>
+        <div className="p-5">
+          <h1 className="text-[20px] font-inter font-bold">{heading}</h1>
+          <p>{para}</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ServiceCard
+export default ServiceCard;
