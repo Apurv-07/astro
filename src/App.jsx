d35c9a6a8bc911fae629ddc6859ff1b959af973a
KARTIK9 <ktidke3@gmail.com>
2023-08-24 12:35:34 +0530

    about page zodiac cards collapse and expand functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,56 @@
 // import React from 'react'
-import Navigation from './components/Navigation'
+import { useEffect, useState } from "react";
+import Navigation from "./components/Navigation";
+import { BiSolidUpArrowAlt } from "react-icons/bi";
+import { motion } from "framer-motion";
+
 const App = () => {
+  const [moveToTopBtnVisible, setMoveToTopBtnVisibility] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  //handling move to top button visibility
+  useEffect(() => {
+    const handleMoveToTopBtnVisibility = () => {
+      window.scrollY > 800
+        ? setMoveToTopBtnVisibility(true)
+        : setMoveToTopBtnVisibility(false);
+    };
+
+    window.addEventListener("scroll", handleMoveToTopBtnVisibility);
+
+    return () =>
+      window.removeEventListener("scroll", handleMoveToTopBtnVisibility);
+  }, []);
+
+  // handling click on move to top button
+  const handleClick = () => {
+    window.scroll({ top: 0, behavior: "smooth" });
+    setIsOpen(false)
+  };
+
   return (
     <div>
+      {/* navigation component */}
       <Navigation activeClass={location.pathname} />
-    </div>
-  )
-}
 
-export default App
+      {/* move to top button */}
+      {moveToTopBtnVisible && (
+        <motion.div
+          layout
+          className="fixed right-10 bottom-10 flex justify-center items-center"
+          onClick={handleClick}
+          whileHover={{ cursor: "pointer", userSelect: "none" }}
+          onMouseEnter={() => setIsOpen(true)}
+          onMouseLeave={() => setIsOpen(false)}
+        >
+          <BiSolidUpArrowAlt className="w-10 h-10" />
+          <motion.span className={isOpen ? "block" : "hidden"}>
+            Move to top
+          </motion.span>
+        </motion.div>
+      )}
+    </div>
+  );
+};
+
+export default App;
--- src/components/ZodiacCard.jsx
+++ src/components/ZodiacCard.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { motion } from "framer-motion";
 const ZodiacCard = (props) => {
-  const { img, name, description, date,title } = props;
+  const { img, name, description, date, title } = props;
   const path = "assets/";
+
+  const [isCardExpanded, setIsCardExpanded] = useState(false);
 
   return (
     <>
-      <div className="flex gap-x-6 bg-orange-300  w-[800px]  bg-gradient-to-r from-yellow-400 to-orange-400 drop-shadow-xl overflow-hidden  max-sm:w-full rounded-xl">
+      <motion.div
+        layout
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ amount: 0.3, once: true }}
+        transition={{ type: "just", duration:0.1 }}
+        className={`flex gap-x-6 bg-orange-300 w-[800px]  bg-gradient-to-r from-yellow-300 to-orange-300 drop-shadow-xl max-sm:w-full rounded-xl relative ${
+          isCardExpanded ? "h-auto" : "h-[25rem]"
+        }`}
+      >
         {/* IMAGE */}
         <div className="flex flex-col items-center">
           <div className="w-[300px] h-[300px] max-sm:w-[200px] max-sm:h-[200px]">
@@ -16,17 +30,36 @@ const ZodiacCard = (props) => {
           </div>
           {/* NAME */}
           <div className="mt-4 text-center font-bold">
-
-          <h1 className="text-2xl  ">{name}</h1>
-          <p >{date}</p>
+            <h1 className="text-2xl  ">{name}</h1>
+            <p>{date}</p>
           </div>
         </div>
         {/* DESCRIPTION */}
-        <div className="w-[60%] max-sm:w-[200px] max-sm:h-[200px] p-4 pl-0 ">
-            <h1 className="text-2xl font-bold text-center ">{title}</h1>
-          <p className="text-ellipsis">{description}</p>
+        <motion.div className="w-[60%] max-sm:w-[200px] max-sm:h-[200px] p-4 pl-0 ">
+          <h1 className="text-2xl font-bold text-center ">{title}</h1>
+          <p className={`${!isCardExpanded && "line-clamp-[13]"}`}>
+            {description}
+          </p>
+        </motion.div>
+        <div
+          className={`absolute ${
+            isCardExpanded ? "-bottom-5" : "bottom-2"
+          } right-3 hover:cursor-pointer hover:text-black text-blue-700 flex justify-between items-center`}
+          onClick={() => setIsCardExpanded(!isCardExpanded)}
+        >
+          {isCardExpanded ? (
+            <>
+              read less
+              <IoIosArrowUp />
+            </>
+          ) : (
+            <>
+              read more
+              <IoIosArrowDown />
+            </>
+          )}
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
